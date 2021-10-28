ca2a0b5559e98a6dd8fdff55b04e6cd917350173
Enzo Vernon <enzodante.v@gmail.com>
2021-10-28 16:36:35 -0400

    removed covid-19 analytics project and started java notes

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,15 @@
+// public = an access modifier that defines scope to allow other parts of the code to access this code
+// class = code block(unique code to specific method) with body defined by curly braces and Name with an access modifier
+public class Hello {
+    // main() = the method that serves as the entry point Java looks for when running an app
+    // static = an access modifier
+    // void = the method (a function on a class) will not return anything
+    // String[] args = a parameter that is named args and is an array of Strings
+    public static void main(String[] args){
+
+        // System.out.println() = print statement that uses double quotes
+        // Java requires semicolons to end lines
+        System.out.println("Hello World!");
+    }
+
+}
